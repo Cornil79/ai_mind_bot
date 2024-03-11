2b27ccc77cbd356d7c564c523fff232e6569c516
Cornil79 <cornil@mail.ru>
2024-03-11 22:42:42 +0000

    Независимые промпты для каждого пользователя

--- ai_mind_bot.py
+++ ai_mind_bot.py
@@ -23,7 +23,8 @@ OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 DOC_URL = os.environ.get("DOC_URL")
 
 open_ai_router = OpenAIRouter()
-load_result = open_ai_router.reload_prompts(DOC_URL)
+
+load_result = open_ai_router.load_prompts(DOC_URL, 0)
 if load_result[1]:
     print(f"\n\033[92m{load_result[0]}\033[0m")  # Зелёный
 else:
@@ -37,17 +38,23 @@ faiss_router = FaissRouter()
 # функция-обработчик команды /start 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     global open_ai_router
+    user_id = update.effective_user.id
     # возвращаем текстовое сообщение пользователю
     hello_text = 'Классификатор запросов "Планета Здоровья"'
     first_message = await update.message.reply_text(hello_text)
 
-    load_result = open_ai_router.reload_prompts(DOC_URL)
+    load_result = open_ai_router.load_prompts(DOC_URL, user_id)
     if load_result[1]:
         print(f"\n\033[92m{load_result[0]}\033[0m")  # Зелёный
     else:
         print(f"\n\033[91m{load_result[0]}\033[0m")  # Красный
 
-    prompts_url = f"<b>Ссылка для редактирования промптов:</b>\n{DOC_URL}\n\nПосле редактирования, еще раз используйте команду /start"
+    prompts_url = f"""<b>Ссылка на исходные промпты для OpenAI:</b>\n{DOC_URL}\n
+✅ Для смены промптов, скопируйте документ по ссылке выше и используйте команду /setdocurl <i>current_doc_url</i>
+Где <i>current_doc_url</i> - ссылка на новый документ с промптами.\n
+<b>ℹ️ Внимание, <i>current_doc_url</i> не сохраняется в файл и при перезапуске бота на сервере он будет сброшен.</b>\n
+ℹ️ Для загрузки стандартных промптов, еще раз используйте команду /start"""
+    
     await context.bot.edit_message_text(
         text=f"{hello_text}\n\n{load_result[0]}\n\n{prompts_url}",
         chat_id=update.message.chat_id,
@@ -56,10 +63,32 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         disable_web_page_preview=True
     )
 
+# Функция-обработчик команды /setdocurl
+async def set_doc_url(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.effective_user.id
+    args = context.args  # Получаем список аргументов
+    if args:
+        current_doc_url = args[0]  # Первый аргумент после команды считается URL
+        load_result = open_ai_router.load_prompts(current_doc_url, user_id=user_id)
+        response_text = f"URL для загрузки промптов обновлён: {current_doc_url}\n\n<b>Результат загрузки:</b>\n{load_result[0]}"
+    else:
+        response_text = "Пожалуйста, укажите URL после команды. Например: /setdocurl http://example.com/prompts.md"
+    
+    await update.message.reply_text(response_text, parse_mode='HTML', disable_web_page_preview=True)
+
+async def reload(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.effective_user.id
+    # Вызов функции перезагрузки промптов
+    result_message = open_ai_router.reload_prompts(user_id)
+    
+    # Отправка результата пользователю
+    await update.message.reply_text(result_message[0], parse_mode='HTML')
+
 # функция-обработчик текстовых сообщений
 async def text(update: Update, context: ContextTypes.DEFAULT_TYPE):
     global faiss_router
     global open_ai_router
+    user_id = update.effective_user.id
     # вывод сообщения об обработке документа
     first_message = await update.message.reply_text('Вопрос принят...')
 
@@ -69,26 +98,35 @@ async def text(update: Update, context: ContextTypes.DEFAULT_TYPE):
     end_faiss_time = round(time.time() - start_faiss_time, 6)
 
     start_ai_time = time.time()
-    res = await open_ai_router.get_route(question = update.message.text)
+    res = await open_ai_router.get_route(question=update.message.text, user_id=user_id)
     end_ai_time = round(time.time() - start_ai_time, 6)
+    prompt = open_ai_router.prompts.get(user_id)
+    if prompt is not None:
+        if prompt['url'] == DOC_URL:
+            prompt_info = f"❗️<i><b>Используется стандартный промпт</b></i>"
+        else:
+            prompt_info = f"ℹ️ Промпт обновлен: {prompt['last_update']}"
+    else:
+        prompt_info = f"❗️<i><b>Используется стандартный промпт</b></i>"
 
     #faiss_result = f"\n<b>FAISS:</b>\n<i>{faiss_res[0]['cluster_name']}</i>\nУверен: <i>{(faiss_res[0]['stars_count']/faiss_res[0]['stars_total'])*100}%</i>"
     faiss_result = f"\n<b>FAISS</b> ({end_faiss_time} секунд):\n<i>{faiss_res[0]['cluster_name']}</i>\nУверен: <i>{faiss_res[0]['stars_count']}/{faiss_res[0]['stars_total']}</i>"
-    open_ai_result = f"\n<b>OpenAI</b> ({end_ai_time} секунд):\n<i>{res}</i>"
+    open_ai_result = f"\n<b>OpenAI</b> ({end_ai_time} секунд):\n{prompt_info}\n<i>{res}</i>"
 
     end_time = round(end_faiss_time + end_ai_time, 6)
-    message_text = f"Запрос выполнен за: <b>{end_time}</b> секунд\n{faiss_result}\n{open_ai_result}"
+    message_text = f"Запрос выполнен за: <b>{end_time}</b> секунд\n{faiss_result}\n{open_ai_result}\n\nПерезагрузить промпт /reload"
     # сообщение результата
     await context.bot.edit_message_text(text = message_text, chat_id = update.message.chat_id, message_id = first_message.message_id, parse_mode='HTML')
 
 def main():
-    # создаем приложение и передаем в него токен бота
     application = Application.builder().token(TOKEN).build()
+
+    application.add_handler(CommandHandler('start', start))
+    application.add_handler(CommandHandler('setdocurl', set_doc_url))
+    application.add_handler(CommandHandler('reload', reload))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, text))
+
     print('\n\033[92mБот запущен...\033[0m')
-    # добавление обработчиков
-    application.add_handler(CommandHandler('start', start, block=False))
-    application.add_handler(MessageHandler(filters.TEXT, text, block=False))
-    # запуск бота (нажать Ctrl+C для остановки)
     application.run_polling()
     print('Бот остановлен')
 
--- open_ai_router.py
+++ open_ai_router.py
@@ -1,19 +1,37 @@
 import openai
 #from config import route_form, system, user_footer
 import requests
+import os
+from datetime import datetime
+import pytz
+
+# Создаём объект временного пояса для Москвы
+moscow_tz = pytz.timezone('Europe/Moscow')
 
 
 class OpenAIRouter():
     def __init__(self):
+        self.DOC_URL = os.environ.get("DOC_URL")
+        moscow_time = datetime.now(moscow_tz)
         self.prompts = {
-            'system': '',
-            'user_footer': '',
+            '0': {
+                'url': self.DOC_URL,
+                'system': '',
+                'user_footer': '',
+                'route_form': '',
+                'last_update': moscow_time.strftime('%Y-%m-%d <b>%H:%M:%S</b>')
+            }
         }
+        # self.reload_prompts(self, self.DOC_URL, 0)
 
-    async def get_route(self, question: str):
-        user = f"Вопрос пользователя: {question}\nФорма_000: {self.prompts['route_form']}\n{self.prompts['user_footer']}"
+    async def get_route(self, question: str, user_id):
+        user_prompts = self.prompts.get(user_id)
+        if user_prompts is None:
+            user_prompts = self.prompts.get(0)
+
+        user = f"Вопрос пользователя: {question}\nФорма_000: {user_prompts['route_form']}\n{user_prompts['user_footer']}"
         messages = [
-            {"role": "system", "content": self.prompts['system']},
+            {"role": "system", "content": user_prompts['system']},
             {"role": "user", "content": user}
         ]
         completion = openai.ChatCompletion.create(model='gpt-3.5-turbo-16k', messages=messages, temperature=0)
@@ -30,16 +48,32 @@ class OpenAIRouter():
         # Это может включать в себя анализ "score" или других параметров для определения релевантности
         return answer  # Пока возвращает оригинальный ответ без изменений
 
-    def reload_prompts(self, export_url):
-        export_url = export_url.replace("/edit", "/export?format=txt")
-        response = requests.get(export_url)
+    def reload_prompts(self, user_id):
+        prompt = self.prompts.get(user_id)
+        if prompt is not None:
+            return self.load_prompts(prompt['url'], user_id)
+        else:
+            return [f"❌ Не могу найти ссылку на промпт"]
+
+    def load_prompts(self, export_url, user_id):
+        if "/edit" in export_url:
+            index_of_edit = export_url.find("/edit") + len("/edit")
+            load_url = export_url[:index_of_edit]
+            load_url = load_url.replace("/edit", "/export?format=txt")
+        else:
+            print("URL не содержит '/edit'")
+
+        response = requests.get(load_url)
         # Проверяем, успешно ли выполнен запрос
         if response.status_code == 200:
             document_text = response.text
             #print(document_text)
             prompts = load_markdown_to_dict(document_text)
             if all(key in prompts for key in ['system', 'user_footer', 'route_form']):
-                self.prompts = prompts
+                moscow_time = datetime.now(moscow_tz)
+                prompts['last_update'] = moscow_time.strftime('%Y-%m-%d <b>%H:%M:%S</b>')
+                prompts['url'] = export_url
+                self.prompts[user_id] = prompts
                 return ["✅ Промпты и форма_000 успешно загружены", True]
             else:
                 print(prompts)
